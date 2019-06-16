46c1f7a31d45b8e7346b4050a11d13d4ee2d9946
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-16 10:46:37 +0530

    Create RecursionPractice.py

---
+++ Lesson3/RecursionPractice.py
@@ -0,0 +1,14 @@
+"""Implement a function recursively to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions."""
+
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    return get_fib(position - 1) + get_fib(position - 2)
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
